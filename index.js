a7d86ee2869826e4653c78c17bc443de03d62f82
Lasse Haverinen <lasse.haverinen@gmail.com>
2016-10-14 15:03:18 +0300

    Server implementation

---
+++ index.js
@@ -0,0 +1,46 @@
+// bring in the necessary dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// Configure the express web server framework
+var app = express();
+app.use(bodyParser.json());
+
+// Define the API routes for our application
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var messages = [
+    {
+        username: "Mr. A",
+        message: "Hello world"
+    },
+    {
+        username: "Mr. B",
+        message: "Good day"
+    }
+
+];
+
+//
+// /messages
+// get
+// post
+app.get('/messages', function (req, res) {
+    res.json(messages);
+});
+
+app.post('/messages', function (req, res) {
+    messages.push({
+        username: req.body.username,
+        message: req.body.message
+    });
+    res.json(messages);
+});
+
+
+// Activate the express server
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
