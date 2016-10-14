296a5f5386ed1271cc07307d0810e5654ce55ea4
Lasse Haverinen <lasse.haverinen@gmail.com>
2016-10-14 15:11:33 +0300

    Changed port to be heroku compatible

--- index.js
+++ index.js
@@ -41,6 +41,6 @@ app.post('/messages', function (req, res) {
 
 
 // Activate the express server
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening');
 });
